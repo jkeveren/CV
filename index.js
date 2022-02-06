4049387e7c94f3d7979772771cee01b4227aa0c8
James Keveren <james@keve.ren>
2022-02-06 02:41:47 +0000

    personal projects

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function anchor(parent, text, href) {
 function heading(parent, text) {
 	const h = div(parent);
 	Object.assign(h.style, {
-		fontSize: "2em",
+		fontSize: "1.7em",
 		fontWeight: "bold",
 		marginTop: "15px",
 	})
@@ -26,7 +26,7 @@ function heading(parent, text) {
 function heading2(parent, text) {
 	const h = heading(parent, text);
 	Object.assign(h.style, {
-		fontSize: "1.5em",
+		fontSize: "1.2em",
 		marginTop: "5px",
 	});
 	return h;
@@ -65,14 +65,12 @@ Object.assign(document.body.style, {
 	const container = div(document.body);
 	Object.assign(container.style, {
 		maxWidth: "680px",
-		width: "100vw",
 	})
 
 	{ // header
 		const header = div(container);
 		Object.assign(header.style, {
 			display: "flex",
-			flexDirection: "row",
 			gap: "10px",
 			alignItems: "center",
 			paddingBottom: "5px",
@@ -137,17 +135,17 @@ Object.assign(document.body.style, {
 
 	// summary
 	heading(container, "Summary");
-	text(container, `
-Software Engineer with experience in all aspects of building software including design, implementation, and deployment.
-Most of my experience has been building REST APIs, microservices and frontends in JS, Go and C# using both SQL and No-SQL DBs.
-I also have experience with a wide range of other technologies as listed in the section below.
-I think that it's important to follow best practices in order to promote and retain readability, maintainablilty and reliability.
-	`);
+	text(container,
+		`Software Engineer with experience in all aspects of building software including design, implementation, and deployment.
+		Most of my experience has been building REST APIs, microservices and frontends in JS, Go and C# using both SQL and No-SQL DBs.
+		I also have experience with a wide range of other technologies as listed in the section below.
+		I think that it's important to follow best practices in order to promote and retain readability, maintainablilty and reliability.`
+	);
 
 	// skills and technology
 	// items are stored in an array to for easier reordering
 	heading(container, "Skills and Technology");
-	const groups = [
+	const skillSets = [
 		["Software Engineering", [
 			"Golang",
 			"JavaScript",
@@ -229,9 +227,111 @@ I think that it's important to follow best practices in order to promote and ret
 			"Gimp",
 			"Paint.net",
 		]]
+	];
+	for (let set of skillSets) {
+		heading2(container, set[0]);
+		text(container, set[1].join(", "));
+	}
+
+	// Employment
+	heading(container, "Employment");
+	const IFLMHeadRow = div(container);
+	Object.assign(IFLMHeadRow.style, {
+		display: "flex",
+		justifyContent: "space-between",
+	});
+	heading2(IFLMHeadRow, "IFL Management");
+	text(IFLMHeadRow, "Nov 2017 - Present");
+	text(container, "IFLM text...");
+
+	// personal projects
+	heading(container, "Personal Projects");
+	const personalProjects = [
+		[
+			"Crop Collage",
+			"github.com/jkeveren/crop-collage",
+			"Nov 2021",
+			["C++", "Magick++"],
+			`Linux tool that recursively finds images with an xattr that specifies an ImageMagic geometry.
+			Crops to that geometry and builds a collage from those cropped images.
+			Allows the archival of original images and use of cropped image in collage without the necessity to store a cropped version.
+			Optimized to reduce memory usage.`,
+		],
+		[
+			"Personal Website",
+			"github.com/jkeveren/personal-website",
+			"Mar 2015 - present",
+			["Go", "REST", "TDD"],
+			`Manipulates HTTP connections to "trickle" the home page.
+			Includes a gallery feature that is optimised for serving images over a connection with limited bandwidth using compression and caching.`,
+		],
+		[
+			"Crossout Market Scoring Tool",
+			"github.com/jkeveren/crossout-market-scoring-tool",
+			"Aug 2020",
+			["Node.js"],
+			`Tool that analyses the market in the game Crossout in order to find the best items to buy and sell for profit.
+			Takes into account supply and demand, market activity and ROI.`,
+		],
+		[
+			"Doogle",
+			"github.com/jkeveren/doogle",
+			"Jun 2020 and Jan 2021",
+			["Go", "Regexp", "Node.js", "Firebase"],
+			`Proxy to Google.com which replaces every instance of the word "Google" with the word "Doogle" including logos.
+			Any search is replaced by an image search for "beagle" with all instances of "beagle" replaces by "doogle".`,
+		],
+		[
+			"File Drop",
+			"github.com/jkeveren/file-drop",
+			"Dec 2021",
+			["Go", "JavaScript", "HTML"],
+			`Simple website for uploading files.
+			Useful when someone wants to send me a large file without an FTP client.`,
+		],
+		[
+			"Home Controller",
+			"github.com/jkeveren/home",
+			"Sept 2019 - Jul 2020",
+			["Node.js"],
+			`used to control various lights and appliances around my home using Sonoff smart switches via their HTTP API.`,
+		],
+		[
+			"MassDraw",
+			"github.com/jkeveren/massdraw",
+			"Aug - Oct 2017",
+			["Node.js", "Socket.io"],
+			`Allows multiple people to draw on a shared whiteboard in realtime using Socket.io and JavaScript's canvas API.`,
+		],
 	]
-	for (let group of groups) {
-		heading2(container, group[0]);
-		text(container, group[1].join(", "))
+	for (let p of personalProjects) {
+		const [name, link, date, skills, description] = p;
+		
+		const row = div(container);
+		Object.assign(row.style, {
+			display: "flex",
+			gap: "10px",
+			alignItems: "flex-end",
+		});
+
+		heading2(row, name).style.flexGrow = 1;
+
+		const skillsContainer = div(row);
+		Object.assign(skillsContainer.style, {
+			display: "flex",
+			gap: "4px"
+		});
+		for (let s of skills) {
+			const e = text(skillsContainer, s);
+			Object.assign(e.style, {
+				background: "#ccc",
+				borderRadius: "4px",
+				padding: "0 2px",
+			});
+		}
+
+		anchor(row, link, "https://" + link);
+		text(row, date);
+		text(container, description);
 	}
 }
